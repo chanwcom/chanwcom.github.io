29fb517d5ab6b44e6b3b0dfdc573b52e66a32b96
Chanwoo Kim <chanwcom@gmail.com>
2024-07-07 12:44:59 +0900

    Update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Keras model definition",
+        "excerpt":"In this page, we discuss how to define a model using Keras.  ","categories": [" -","Github_Blog"],
+        "tags": [" -","minimal","mistakes"," -","jekyll"," -","blog"],
+        "url": "/%C2%A0-/github_blog/keras_model/",
+        "teaser": "/assets/images/chanwoo_kim_fb.jpg"
+      },{
+        "title": "[Github Blog] Jekyll - minimal mistakes 시작하기",
+        "excerpt":"This is a test page.  ","categories": [" -","Github_Blog"],
+        "tags": [" -","minimal","mistakes"," -","jekyll"," -","blog"],
+        "url": "/%C2%A0-/github_blog/tensorflow_fft/",
+        "teaser": "/assets/images/chanwoo_kim_fb.jpg"
+      }]
